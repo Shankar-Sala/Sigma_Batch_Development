5cee9eb43c56b953db3c4832674fe45822f250e1
Shankar-Sala <shankarsala9995@gmail.com>
2024-12-03 14:43:41 +0530

    assignment-4

---
+++ JS P-4/assignment Qs/Qs-2.js
@@ -0,0 +1,13 @@
+// Qs2. Write a JS program to find the no of digits in a number.Example: if number = 287152, count = 6
+
+let number = 287152;
+
+let count = 0;
+let copy = number;
+
+while(copy > 0){
+  count++;
+  copy=Math.floor(copy/10);   //Math.floor();
+}
+
+console.log(count);
